dbedd67e7f4858f7a5d38ddde6c3c4fcc90ab71c
donal carpenter <donal.carpenter@yahoo.com>
2013-05-09 22:05:49 +0100

    adding excerice 3 for http server

---
+++ chapter4/excercise3.js
@@ -0,0 +1,45 @@
+var http = require('http');
+var fs = require('fs');
+var util = require('util');
+
+var finalize = function (res, status, content){
+	res.writeHead(status, {'Content-Type': 'text/plain'});
+	res.end(content);
+}
+
+http.createServer(function(req, res){
+	var filepath = '.' + req.url;
+
+	fs.open(filepath, 'w+', function(err, fd){
+
+		if(err){
+			finalize(res, 500, util.inspect(err));
+		}
+
+		req.on('data', function(chunk){
+			var totalBytesWritten = 0;
+			console.log('about to write: ' + chunk.length + ' bytes');
+			
+			(function writeOutRequestBody(){
+
+				if(totalBytesWritten < chunk.length){
+					fs.write(fd, chunk, totalBytesWritten, chunk.length, totalBytesWritten, function(err, written){
+
+						if(err){
+							finalize(res, 500, util.inspect(err));
+						}
+
+						totalBytesWritten += written;
+						writeOutRequestBody(chunk);
+
+						var result = 'success, ' + totalBytesWritten + ' written to ' + filepath;
+						console.log(result);
+						
+						finalize(res, 200, result);
+					});
+				}
+				
+			})();
+		});
+	});
+}).listen(4000);
