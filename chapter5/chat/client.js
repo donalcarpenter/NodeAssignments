e3257f9f405cdbca914d9f1f34c6b1556da46e15
donal carpenter <donal.carpenter@yahoo.com>
2013-05-14 07:54:09 +0100

    chat room now functional

--- chapter5/chat/client.js
+++ chapter5/chat/client.js
@@ -6,16 +6,38 @@ client.on('data', function(data){
 	console.log(data.toString());
 });
 
+
+
 var readline = require('readline');
+
+var userid;
 
 rl = readline.createInterface(process.stdin, process.stdout);
 
-rl.setPrompt('say something >');
+rl.setPrompt('username:');
 rl.prompt();
+
+var registered  = false;
 
 rl.on('line', function(line){
 
-	client.write(line);
+	var operation = {};
+
+	if(!registered){
+		console.log('registering');
+		userid = line;
+		operation.user = line;
+		operation.action = "register";
+		registered = true;
+
+		rl.setPrompt(userid + '>');
+	}else{
+		operation.user = userid;
+		operation.action = "chat";
+		operation.data = line; 
+	}
+	
+	client.write(JSON.stringify(operation));
 
 	rl.prompt();
 });
--- chapter5/chat/server.js
+++ chapter5/chat/server.js
@@ -5,15 +5,32 @@ var sockets = [];
 
 net.createServer(function(socket){
 
-	console.log('new connection : ' + util.inspect(socket));
-
-	sockets[sockets.length] = socket;
+	console.log('new connection');
 
 	socket.on('data', function(data){
+
+		var jdata = JSON.parse(data);
+
+		var message = "";
+
+		if(jdata.action == "register"){
+			var registration = {user: jdata.user, socket: socket}
+			sockets[jdata.user] = registration;
+			message = registration.user + ' has joined';
+		}else{
+			message = jdata.user + ': ' + jdata.data;
+		}
+
 		console.log('echoing ' + data.toString());
 
-		for(i = 0; i < sockets.length; i++){
-			sockets[i].write('echo: ' + data.toString());			
+		for(var registration in sockets){
+			if(sockets[registration].socket === socket){
+				if(jdata.action == "register"){
+					socket.write('registration complete, welcome ' + jdata.user);
+				}
+			}else{
+				sockets[registration].socket.write(message);			
+			}
 		}
 	
 	})
